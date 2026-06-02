46516ccbdc38d4a6a0c8839bfd2240d19f030d7b
jorgealbertogonzalez158-droid <jorgealbertogonzalez158@gmail.com>
2026-06-02 09:49:25 -0600

    Add conditional greeting based on user name

---
+++ practica 9/condicionales2.py
@@ -0,0 +1,13 @@
+print("Escribe tu nombre:")
+nombre = input()
+
+#elif 
+
+
+if nombre == "Jorge":
+    print("Saludo Jorge")
+elif nombre == "Jorge":
+    print("Que bonito nombre Jorge")
+
+else:
+    print("Que extraño nombre")
