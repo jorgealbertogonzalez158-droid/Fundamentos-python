1e8317e98352f5c5b9f1e520fd217654f9640b22
jorgealbertogonzalez158-droid <jorgealbertogonzalez158@gmail.com>
2026-06-02 09:26:46 -0600

    Add basic store welcome and product details

---
+++ practica 3/tienda.py
@@ -0,0 +1,14 @@
+print("*****************************************")
+print("**             BIENVENIDO A            **")
+print("**          LA TIENDA DE JORGE     ** ")
+print("*****************************************")
+
+num_churos = 2
+num_refresco = 1.50
+num_paletas = 3
+
+Producto_totales = num_churos + num_refresco + num_paletas
+
+print("Actualmente contamos con:")
+print("churos:",num_churos, "refresco:", num_refresco,"paletas", num_paletas)
+print("En total tenemos", Producto_totales, "producto")
