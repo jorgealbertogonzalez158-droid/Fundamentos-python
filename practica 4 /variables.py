906b99c26f04f2749d9d302182c9eda854a23aa9
jorgealbertogonzalez158-droid <jorgealbertogonzalez158@gmail.com>
2026-06-02 09:31:06 -0600

    Add variables and print statements in variables.py

---
+++ practica 4 /variables.py
@@ -0,0 +1,14 @@
+#Soy un comentario
+#Strings, cadenas, textos
+#numericos int  (integer, entero) float (punto flotante)
+nombre = "Jorge"
+apellido = "González"
+edad = "17"
+
+print(nombre, apellido, edad)
+
+#edad = edad /3
+
+print(nombre, apellido, edad)
+
+print(type(edad))
