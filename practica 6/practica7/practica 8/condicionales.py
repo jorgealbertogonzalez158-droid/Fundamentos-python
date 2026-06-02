f7b7579d067f27da4e4676d9d0fd6366eb4d316b
jorgealbertogonzalez158-droid <jorgealbertogonzalez158@gmail.com>
2026-06-02 09:46:21 -0600

    Create condicionales.py

---
+++ practica 6/practica7/practica 8/condicionales.py
@@ -0,0 +1,12 @@
+#Lógico boolean booleano bool 
+#verdadero / Falso: True / False 
+##dato = 5 < 5
+#< > ==
+
+print("Ingresa tu nombre")
+nombre = input()
+
+if nombre == "Jorge":
+    print("Bienvenido Jorge")
+else:
+    print("Que nombre tan extraño")
